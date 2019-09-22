c132982be3eec78a0a60235e91e183e70063ec45
FLI\nbaynham <nbaynham@flhi.com>
2019-09-22 11:39:19 -0400

    updated to use MicrosoftWebDriver instead of Edge

--- src/test/java/examples/ExampleTest.java
+++ src/test/java/examples/ExampleTest.java
@@ -2,15 +2,32 @@ package examples;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class ExampleTest {
-    WebDriver driver;
+    private WebDriver driver;
 
     @Test
-    public void test() {
+    public void testChrome() {
         System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.quit();
+    }
+
+    @Test
+    public void testFirefox() {
+        System.setProperty("webdriver.gecko.driver", "C:\\webdriver\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.quit();
+    }
+
+    @Test
+    public void testEdge() {
+        System.setProperty("webdriver.edge.driver", "C:\\webdriver\\MicrosoftWebDriver.exe");
+        driver = new EdgeDriver();
         driver.quit();
     }
 }
